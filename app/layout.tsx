d44033ff63050c2d55666662cd5483ffab9608d3
HeyyyNeo <shubhamchaudhary010259@gmail.com>
2024-07-21 21:06:51 +0530

    seo changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import WithThemeLayout from "./theme";
+import Head from "next/head";
 
 export const metadata: Metadata = {
-  title: "Alphabet Analytics",
+  title: "Alphabet Analytix",
   description: "Analytics software to manage students",
 };
 
@@ -14,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="canonical" href="https://www.alphabetanalytix.com/" />
+      </head>
       <body className="root">
         <WithThemeLayout>{children}</WithThemeLayout>
       </body>
